f8b06a8847f16eeaf400a3daff63f9f761a58f4c
sspdev <kolybelnaya27ss@gmail.com>
2023-01-24 07:46:15 +0800

    added new lines in JustClass2

--- src/main/java/JustClass2.java
+++ src/main/java/JustClass2.java
@@ -3,5 +3,8 @@ public class JustClass2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world");
+        //new line there
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
