83c3bbf1df2c8af43520ba48e61dd4b6fca2d704
sspdev <kolybelnaya27ss@gmail.com>
2023-01-29 10:57:11 +0800

    Added new lines in MainClass.java

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,5 +2,10 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
     }
 }
