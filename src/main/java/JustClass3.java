282b24bd89e365e2a746a7df01be7fd36ef5f9d9
sspdev <kolybelnaya27ss@gmail.com>
2023-01-29 11:13:15 +0800

    deleted some lines from JustClass3

--- src/main/java/JustClass3.java
+++ src/main/java/JustClass3.java
@@ -2,13 +2,5 @@ public class JustClass3 {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
     }
 }
